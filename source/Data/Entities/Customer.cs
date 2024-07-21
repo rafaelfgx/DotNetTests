0ac59c301e1f4bcc20bb0e3001499a05046ca722
Rafael <rafaelfgx@gmail.com>
2024-07-21 12:01:30 -0300

    .

---
+++ source/Data/Entities/Customer.cs
@@ -0,0 +1,14 @@
+namespace Data;
+
+public sealed record Customer
+{
+    public string Email { get; set; }
+
+    public string FirstName { get; set; }
+
+    public int Id { get; set; }
+
+    public string LastName { get; set; }
+
+    public string Telephone { get; set; }
+}
